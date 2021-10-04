272337096f56eac00680c439da7ba7b7cab61f7a
Evan Riley <evanriley@hey.com>
2021-10-04 16:16:50 -0400

    Fix isntalling on M1 macs (missing homebrew on path)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx PATH ~/bin $NPM_PACKAGES/bin ~/.cargo/bin ~/.emacs.d/bin $PATH
+set -gx PATH ~/bin $NPM_PACKAGES/bin ~/.cargo/bin ~/.emacs.d/bin /opt/homebrew/bin $PATH
 set -gx GPG_TTY (tty)
 
 set -g fish_greeting
