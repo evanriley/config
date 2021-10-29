b64878eef8b87d6b050de64b3ade0d0b71ac989b
Evan Riley <evanriley@hey.com>
2021-10-29 19:05:42 -0400

    Clean up fish config
    its 2021 old man, no one uses `=` anymore.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,47 +6,46 @@ set -g fish_greeting
 
 # Aliases
 # Use this to manage dotfiles
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias config '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-alias vim='nvim'
+alias vim 'nvim'
 
 ## get rid of command not found
-alias cd..='cd ..'
+alias cd.. 'cd ..'
 
 ## a quick way to get out of current directory ##
-alias ..='cd ..'
-alias ...='cd ../../../'
-alias ....='cd ../../../../'
-alias .....='cd ../../../../'
-alias .4='cd ../../../../'
-alias .5='cd ../../../../..'
+alias .. 'cd ..'
+alias ... 'cd ../../../'
+alias .... 'cd ../../../../'
+alias ..... 'cd ../../../../'
+alias .4 'cd ../../../../'
+alias .5 'cd ../../../../..'
 
-alias wget='wget -c'
+alias wget 'wget -c'
 
-##honestly this isn't something that should be used...##
-alias gac='git add . && git commit -a -m '
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb'
-alias ge='git-edit-new'
+alias gac 'git add . && git commit -a -m '
+alias gl 'git pull --prune'
+alias glog "git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp 'git push origin HEAD'
+alias gd 'git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+alias gc 'git commit'
+alias gca 'git commit -a'
+alias gco 'git checkout'
+alias gcb 'git copy-branch-name'
+alias gb 'git branch'
+alias gs 'git status -sb'
+alias ge 'git-edit-new'
 
 ##wtf is my ip address...##
-alias myip="curl http://ipecho.net/plain; echo"
+alias myip "curl http://ipecho.net/plain; echo"
 
 ## more util commands
-alias ls='exa'
-alias l='exa -l'
-alias ll='exa -l | less'
-alias la='exa -la'
-alias lg='exa --git'
-alias cat='bat'
+alias ls 'exa'
+alias l 'exa -l'
+alias ll 'exa -l | less'
+alias la 'exa -la'
+alias lg 'exa --git'
+alias cat 'bat'
 
 if test (uname) = "Darwin"
  alias tmn 'tmux -CC new -A -s main'
@@ -57,7 +56,7 @@ if test (uname) = "Darwin"
 end
 
 # tidy
-alias tidy=/opt/homebrew/bin/tidy
+alias tidy /opt/homebrew/bin/tidy
 
 # pyenv
 status is-login; and pyenv init --path | source
