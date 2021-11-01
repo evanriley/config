105702144cd6a45ae8e0d19a54f929da994a2b1d
Evan Riley <evanriley@hey.com>
2021-11-01 07:30:31 -0400

    Use diaganosticls for formatting. and other stuff.

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -185,9 +185,7 @@
        ;;solidity          ; do you need a blockchain? No.
        swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       (web               ; the tubes
-        +html
-        +css)
+       web               ; the tubes
        yaml              ; JSON, but readable
        zig               ; C, but simpler
 
