d022ee30c574e0928cc5652cf697f56b84015970
Evan Riley <git@evanriley.dev>
2022-01-08 12:34:40 -0500

    refactor(nvim): prepare the nvim-cmp move
    nvim-cmp is setup and ready to go...but it doesn't quite work the way I want it to yet.
    Probably sticking with coq_nvim out of pure laziness at this point.

--- .config/nvim/lua/lsp/lsp-config.lua
+++ .config/nvim/lua/lsp/lsp-config.lua
@@ -29,6 +29,7 @@ end
 
 -- Capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.codeAction = {
 	dynamicRegistration = true,
 	codeActionLiteralSupport = {
@@ -126,6 +127,7 @@ end
 for server, config in pairs(configs) do
     config.capabilities = capabilities
     config.on_attach = on_attach
+    -- lspconfig[server].setup(config)
     lspconfig[server].setup(coq.lsp_ensure_capabilities(config))
 end
 
---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,103 @@
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+local lspkind = require("lspkind")
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+local tabnine = require("cmp_tabnine.config")
+
+local source_mapping = {
+	buffer = "[Buffer]",
+	nvim_lsp = "[LSP]",
+	nvim_lua = "[Lua]",
+	cmp_tabnine = "[TN]",
+	luasnip = "[Snippet]",
+	path = "[Path]",
+}
+
+tabnine:setup({
+  max_lines = 2000;
+  max_num_results = 10;
+  sort = true;
+  run_on_every_keystroke = true;
+  snippet_placeholder = "..";
+})
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    -- Super-tab setup
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
+  sources = cmp.config.sources ({
+    { name = "nvim_lsp" },
+    { name = "cmp_tabnine" },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+  }),
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.kind = lspkind.presets.default[vim_item.kind]
+			local menu = source_mapping[entry.source.name]
+			if entry.source.name == 'cmp_tabnine' then
+				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+					menu = entry.completion_item.data.detail .. ' ' .. menu
+				end
+				vim_item.kind = ''
+			end
+			vim_item.menu = menu
+			return vim_item
+		end
+	},
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline("/", {
+  sources = {
+    { name = "buffer" }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+--[[ cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+}) ]]
