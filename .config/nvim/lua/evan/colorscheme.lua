ea1966436c7546f15eb617ac4954ed8964f57e2c
Evan Riley <git@evanriley.dev>
2022-07-01 22:08:04 -0400

    Switch back to tokyo night theme

--- .config/nvim/lua/evan/colorscheme.lua
+++ .config/nvim/lua/evan/colorscheme.lua
@@ -1,4 +1,5 @@
-local colorscheme = "kanagawa"
+local colorscheme = "tokyonight"
+vim.g.tokyonight_style = "night"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
