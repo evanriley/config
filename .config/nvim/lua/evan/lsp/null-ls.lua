31468e0fcb6c4213dd4e9a586009d677a8d47eb5
Evan Riley <git@evanriley.dev>
2022-07-02 15:04:59 -0400

    We're tmux users again...

--- .config/nvim/lua/evan/lsp/null-ls.lua
+++ .config/nvim/lua/evan/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -10,31 +10,32 @@ local diagnostics = null_ls.builtins.diagnostics
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup({
-  debug = false,
-  sources = {
-    formatting.prettierd.with({
-      extra_filetypes = { "toml" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    }),
-    formatting.stylua,
-    formatting.goimports,
-    formatting.gofmt,
-    diagnostics.write_good,
-  },
+	debug = false,
+	sources = {
+		formatting.prettierd.with({
+			extra_filetypes = { "toml" },
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
+		formatting.stylua,
+		formatting.goimports,
+		formatting.gofmt,
+		diagnostics.gitlint,
+		diagnostics.write_good,
+	},
 
-  -- Null-lsp format on save
-  on_attach = function(client, bufnr)
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-          -- if nvim version < 0.8 use vim.lsp.buf.formatting_sync()
-        end,
-      })
-    end
-  end,
+	-- Null-lsp format on save
+	on_attach = function(client, bufnr)
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+					-- if nvim version < 0.8 use vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
 })
