6b5ff6c4b93b0d72bb679782889fac746cc77d5c
Evan Riley <git@evanriley.dev>
2024-03-16 10:06:26 -0400

    Use frum to manage ruby versions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,7 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 set -gx PATH ~/bin $NPM_PACKAGES/bin ~/.cargo/bin ~/.emacs.d/bin /opt/homebrew/bin $PATH
 set -gx GPG_TTY (tty)
+
+frum init | source
 
 set -g fish_greeting
 # Aliases
