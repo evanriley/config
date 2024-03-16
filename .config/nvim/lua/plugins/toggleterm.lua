6e8d5c62f996e81a3b3261ecc3d19423cdc36ff4
Evan Riley <git@evanriley.dev>
2024-03-16 10:53:57 -0400

    Make things a little transparent, a little blurry

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -4,6 +4,15 @@ local autoload = _local_1_["autoload"]
 local nvim = autoload("nvim")
 local function _2_()
   local toggleterm = require("toggleterm")
-  return toggleterm.setup({open_mapping = "<C-t>"})
+  local function size(term)
+    if (term.direction == "horizontal") then
+      return 15
+    elseif (term.direction == "vertical") then
+      return (vim.o.columns * 0.4)
+    else
+      return nil
+    end
+  end
+  return toggleterm.setup({open_mapping = "<C-t>", direction = "float", insert_mappings = true, shade_terminals = true, float_opts = {border = "single", width = 250, height = 40, winblend = 0}, size = size})
 end
 return {{"akinsho/toggleterm.nvim", config = _2_, lazy = false}}
