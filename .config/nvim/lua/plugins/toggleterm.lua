a23fdef16599efd93ee575ec914b1ce0604d8612
Evan Riley <git@evanriley.dev>
2024-03-16 10:49:49 -0400

    fix toggleterm issues

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,45 @@
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	hidden = true,
+	dir = "git_dir",
+	direction = "float",
+})
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+require("toggleterm").setup({
+	size = function(term)
+		if term.direction == "horizontal" then
+			return 15
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.4
+		end
+	end,
+	open_mapping = [[<c-t>]],
+	hide_numbers = true, -- hide the number column in toggleterm buffers
+	shade_filetypes = {},
+	shade_terminals = true,
+	shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+	start_in_insert = true,
+	insert_mappings = true, -- whether or not the open mapping applies in insert mode
+	persist_size = true,
+	direction = "float",
+	close_on_exit = true, -- close the terminal window when the process exits
+	shell = vim.o.shell, -- change the default shell
+	float_opts = {
+		border = "single",
+		width = 250,
+		height = 40,
+		winblend = 3,
+		highlights = {
+			border = "Normal",
+			background = "Normal",
+		},
+	},
+})
+
+-- Open lazygit with toggleterm
+vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
