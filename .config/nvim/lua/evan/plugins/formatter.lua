869eaae912a8e98b4283ac86e43ab29e26248d39
Evan Riley <git@evanriley.dev>
2024-03-16 10:50:48 -0400

    Formatter formatting

--- .config/nvim/lua/evan/plugins/formatter.lua
+++ .config/nvim/lua/evan/plugins/formatter.lua
@@ -1,22 +1,22 @@
 return {
-  "mhartington/formatter.nvim",
-  event = "BufReadPost",
-  config = function()
-    require("formatter").setup({
-      filetype = {
-        go = {
-          require("formatter.filetypes.go").goimports,
-        },
-        elixir = {
-          require("formatter.filetypes.elixir").mixformat
-        },
-        html = {
-          require("formatter.filetypes.html").prettierd
-        },
-        lua = {
-          require("formatter.filetypes.lua").stylua
-       }
-      },
-    })
-  end,
+	"mhartington/formatter.nvim",
+	event = "BufReadPre",
+	config = function()
+		require("formatter").setup({
+			filetype = {
+				go = {
+					require("formatter.filetypes.go").goimports,
+				},
+				elixir = {
+					require("formatter.filetypes.elixir").mixformat,
+				},
+				html = {
+					require("formatter.filetypes.html").prettierd,
+				},
+				lua = {
+					require("formatter.filetypes.lua").stylua,
+				},
+			},
+		})
+	end,
 }
