f6b5caa43ef25a2386609765b9855f2c0c6fa53d
Evan Riley <git@evanriley.dev>
2024-03-16 10:53:09 -0400

    disable vim-go mappings

--- .config/nvim/fnl/plugins/go.fnl
+++ .config/nvim/fnl/plugins/go.fnl
@@ -4,4 +4,6 @@
 [{1 :fatih/vim-go
   :lazy true
   :build ":GoInstallBinaries"
-  :ft [:go]}]
+  :ft [:go]
+  :init (fn []
+          (set nvim.g.go_def_mapping_enabled 0))}]
--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -2,4 +2,8 @@
 local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
 local nvim = autoload("nvim")
-return {{"fatih/vim-go", lazy = true, build = ":GoInstallBinaries", ft = {"go"}}}
+local function _2_()
+  nvim.g.go_def_mapping_enabled = 0
+  return nil
+end
+return {{"fatih/vim-go", lazy = true, build = ":GoInstallBinaries", ft = {"go"}, init = _2_}}
