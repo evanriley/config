c3f89332dc72dce83d78a1db43fbdb0da6c17279
Evan Riley <git@evanriley.dev>
2024-03-16 10:52:43 -0400

    use bob to manage my nvim installations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -42,7 +42,7 @@ end
 set -g simple_ass_prompt_greeting ""
 set -g fish_greeting
 set -gx EDITOR nvim
-set -gx PATH ~/bin ~/.local/bin $NPM_PACKAGES/bin ~/.roswell/bin ~/.yarn/bin ~/.cargo/bin ~/.config/emacs/bin /opt/homebrew/bin  /opt/homebrew/sbin ~/go/bin /opt/homebrew/opt/grep/libexec/gnubin '/Applications/Sublime Text.app/Contents/SharedSupport/bin' $PATH
+set -gx PATH ~/bin ~/.local/bin $NPM_PACKAGES/bin ~/.roswell/bin ~/.yarn/bin ~/.cargo/bin ~/.config/emacs/bin /opt/homebrew/bin  /opt/homebrew/sbin ~/go/bin /opt/homebrew/opt/grep/libexec/gnubin '/Applications/Sublime Text.app/Contents/SharedSupport/bin' /home/evan/.local/share/bob/nvim-bin $PATH
 set -gx GPG_TTY (tty)
 ## use asdf direnv plugin and hook into it
 direnv hook fish | source
