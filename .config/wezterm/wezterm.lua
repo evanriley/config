1a857936d5d87b4bbf2e855ff19158730406a608
Evan Riley <git@evanriley.dev>
2024-03-16 10:49:20 -0400

    Track wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+
+return {
+  font = wezterm.font_with_fallback({
+    "JuliaMono",
+    "JetBrainsMono Nerd Font Mono Regular",
+  }),
+  font_size = 14.0,
+  color_scheme = "kanagawabones",
+}
