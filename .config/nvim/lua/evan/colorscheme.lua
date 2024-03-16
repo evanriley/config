fc2bc338276a7d140370d022e90576ba9fd76b0b
Evan Riley <git@evanriley.dev>
2024-03-16 10:49:32 -0400

    Gruvbox-material all the things

--- .config/nvim/lua/evan/colorscheme.lua
+++ .config/nvim/lua/evan/colorscheme.lua
@@ -1,4 +1,10 @@
-local colorscheme = "kanagawa"
+vim.o.background = "dark"
+-- Gruvbox-Material colorScheme settings
+vim.g.gruvbox_material_background = "medium" -- Set contrast: 'hard', 'medium', 'soft'
+vim.g.gruvbox_material_foreground = "material" -- Foreground color palette: 'material', 'mix', 'original'
+vim.g.gruvbox_material_better_performance = 1 -- For better performance
+vim.g.gruvbox_material_enable_bold = 1 -- Bold in function name
+local colorscheme = "gruvbox-material"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
