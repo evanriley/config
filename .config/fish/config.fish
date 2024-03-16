d783da29cc5e45657be377b319d1e28ff08f11a8
Evan Riley <git@evanriley.dev>
2024-03-16 10:04:23 -0400

    Yes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -gx PATH ~/bin $NPM_PACKAGES/bin ~/.cargo/bin ~/.emacs.d/bin $PATH
+
 set -g fish_greeting
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias vim='nvim'
 
 starship init fish | source
